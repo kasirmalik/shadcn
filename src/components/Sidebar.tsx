6ada8f35393a2ad253bb477e89757ceb9e83ccc8
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-02-01 22:25:08 +0530

    refactor Sidebar component to use Command component for menu structure and improve organization

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,90 @@
-'use client'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import {User} from "lucide-react"
+import {
+  Command as CommandComponent,
 
-import UserItem from "./UserItem"
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+import UserItem from "./UserItem";
+import { ReactNode } from "react";
 
 export default function Sidebar() {
-    return (
-        <>
-        <div className="w-[300px] flex flex-col gap-2 min-w-[300px] border-r min-h-screen p-4">
+    const menuList =[
+        {
+            group:"Genrel",
+            items:[
+                {
+                    link:'/',
+                    icon:<User/>,
+                    text:"Profile"
+                },
+                {
+                    link:'/',
+                    text:"Billing"
+                },
+                {
+                    link:'/',
+                    text:"Notfications"
+                },
+               
+            ],
+
+        },
+        {
+            group:"Settings",
+            items:[
+                {
+                    link:'/',
+                    text:"Genrel Settings"
+                },
+                {
+                    link:'/',
+                    text:"Privacy"
+                },
+                {
+                    link:'/',
+                    text:"Logs"
+                },
+                
+            ],
+
+        }
+    ]
+  return (
+    <>
+      <div className="w-[300px] flex flex-col gap-2 min-w-[300px] border-r min-h-screen p-4">
         <div>
-           <UserItem/> 
+          <UserItem />
         </div>
-        <div className="grow">Menu</div>
+        <div className="grow">
+          <CommandComponent>
+            <CommandList>
+             { menuList.map((menu:any,key:number)=> (
+             <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option: any,optionKey:number)=> 
+                  <CommandItem key={optionKey}>
+                      {option.icon}
+                     {option.text}
+                     </CommandItem>
+                    
+                )}
+              </CommandGroup>
+              
+              ))}
+            </CommandList>
+          </CommandComponent>
+        </div>
         <div>Setting /Notfications</div>
-        </div>
-        
-        </>
-    )
+      </div>
+    </>
+  );
 }
