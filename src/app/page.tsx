60d593e613f6e749ab54077d4cf2d07fb2d9600c
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-02-07 17:28:39 +0530

    add @tanstack/react-table dependency; implement TableDemo component for invoice display; update Home layout to include TableDemo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,25 @@
 import Calender from "@/components/Cards/Calender";
 import Generel from "@/components/Cards/Generel";
 import Lines from "@/components/Cards/Lines";
+import { TableDemo } from "@/components/Cards/Table";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="grid gap-[32px]">
-      <div className="grid grid-cols-2 gap-[32px]">
+      <div className="grid grid-cols-2 gap-[32px] ">
         <Generel />
         <div className="grid gap-[32px]">
           <Calender />
           <Calender />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-[32px]">
+      <div className="grid grid-cols-3 gap-[32px] h-[300px] overflow-hidden">
          <Lines/>
-         <Card className="h-[300px]"/>
-         <Card className="h-[300px]"/>
+         <Card className="p-[32px] overflow-y-scroll">
+          <TableDemo/>
+         </Card>
+         <Card/>
       </div>
     </div>
   );
